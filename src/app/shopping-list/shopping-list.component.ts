c90310c270e626b43a049b1bb73fa98dbb8ae829
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-05-03 15:23:44 +0200

    New ingredients can be added and displayed now.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,10 @@ export class ShoppingListComponent {
     new Ingredient('Chocolate', 250),
     new Ingredient('Flour', 500),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    // Both ways work, not sure if there's any diff between them, second one seems faster and more easily readable
+    // this.ingredients.push(new Ingredient(ingredient.name, ingredient.amount));
+    this.ingredients.push(ingredient);
+  }
 }

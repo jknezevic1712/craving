6c1ff7aa26a1d080405afb885693e526b89856af
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-05-03 15:14:04 +0200

    Connected recipe components and now clicking on recipes show details about them.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'Fudge',
@@ -15,14 +17,18 @@ export class RecipeListComponent {
       'https://www.livewellbakeoften.com/wp-content/uploads/2019/08/Fudge-Recipe-36-s-copy.jpg'
     ),
     new Recipe(
-      'Fudge',
-      'Fudgiest experience ever!',
-      'https://www.livewellbakeoften.com/wp-content/uploads/2019/08/Fudge-Recipe-36-s-copy.jpg'
+      'Ćevapi',
+      'Taste Bosnia!',
+      'https://st2.depositphotos.com/1004288/11083/i/950/depositphotos_110833444-stock-photo-cevapcici-bosnian-minced-meat-kebab.jpg'
     ),
     new Recipe(
-      'Fudge',
-      'Fudgiest experience ever!',
-      'https://www.livewellbakeoften.com/wp-content/uploads/2019/08/Fudge-Recipe-36-s-copy.jpg'
+      'Pork knuckle',
+      'Traditional German dish',
+      'https://s3.envato.com/files/432114257/3349-2023_01_19-15_56_53-ISO200.jpg'
     ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }

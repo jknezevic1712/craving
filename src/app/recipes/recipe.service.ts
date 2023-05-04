56af95dc79b6a2b0dc62886a01d071cb7d5090e0
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-05-04 15:42:50 +0200

    Added ingredients to the recipe.model that are now shown when we click on a recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,17 +10,32 @@ export class RecipeService {
     new Recipe(
       'Fudge',
       'Fudgiest experience ever!',
-      'https://www.livewellbakeoften.com/wp-content/uploads/2019/08/Fudge-Recipe-36-s-copy.jpg'
+      'https://www.livewellbakeoften.com/wp-content/uploads/2019/08/Fudge-Recipe-36-s-copy.jpg',
+      [
+        new Ingredient('Chocolate', 250),
+        new Ingredient('Chocolate chips', 50),
+        new Ingredient('Flour', 300),
+      ]
     ),
     new Recipe(
       'Ćevapi',
       'Taste Bosnia!',
-      'https://st2.depositphotos.com/1004288/11083/i/950/depositphotos_110833444-stock-photo-cevapcici-bosnian-minced-meat-kebab.jpg'
+      'https://st2.depositphotos.com/1004288/11083/i/950/depositphotos_110833444-stock-photo-cevapcici-bosnian-minced-meat-kebab.jpg',
+      [
+        new Ingredient('Spiced minced meat', 250),
+        new Ingredient('Chopped onions', 75),
+        new Ingredient('Lepinja bread', 1),
+      ]
     ),
     new Recipe(
       'Pork knuckle',
       'Traditional German dish',
-      'https://s3.envato.com/files/432114257/3349-2023_01_19-15_56_53-ISO200.jpg'
+      'https://s3.envato.com/files/432114257/3349-2023_01_19-15_56_53-ISO200.jpg',
+      [
+        new Ingredient('Pork knuckle', 1),
+        new Ingredient('Cabbage salad', 100),
+        new Ingredient('Cooked potato slices', 200),
+      ]
     ),
   ];
 
